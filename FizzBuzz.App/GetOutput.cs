ae560e3130a4bf2861e9be913ea10709b3bb6dfe
danilo.afonseca <dnilo@hotmail.com>
2018-01-18 00:43:36 +0000

    Revert "Version 1 to 100"
    This reverts commit 4d27a60a8451f2900fb8e7e961042a11e60ec45c.

--- FizzBuzz.App/GetOutput.cs
+++ FizzBuzz.App/GetOutput.cs
@@ -6,17 +6,7 @@ namespace FizzBuzz.App
     {
         public string Get(int value)
         {
-            var disibleBy3 = (value % 3 == 0);
-            var disibleBy5 = (value % 5 == 0);
-
-            return (disibleBy3 && disibleBy5)
-                ? "FizzBuzz"
-                : (disibleBy3)
-                    ? "Fizz"
-                    : (disibleBy5)
-                        ? "Buzz"
-                        : value.ToString();
+            throw new NotImplementedException();
         }
-            
     }
 }
--- FizzBuzz.App/Program.cs
+++ FizzBuzz.App/Program.cs
@@ -4,19 +4,14 @@ namespace FizzBuzz.App
 {
     internal class Program
     {
-        // version from 1 to 100
         private static void Main(string[] args)
         {
             IGetOutput output = new GetOutput();
 
-            for (var i = 1; i <= 100; i++)
+            for (var i = 0; i < 100; i++)
             {
                 Console.WriteLine(output.Get(i));
             }
-            Console.Write("Press any key to end");
-            Console.ReadKey();
         }
-
-        
     }
 }
